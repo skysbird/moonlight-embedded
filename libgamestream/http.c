c7b9da3951f378eb40d7d830754bb380ad4c937e
skysbird <skysbird@gmail.com>
2025-04-15 12:47:56 +0800

    reuse

--- libgamestream/http.c
+++ libgamestream/http.c
@@ -73,9 +73,10 @@ int http_init(const char* keyDirectory, int logLevel) {
 int http_request(char* url, PHTTP_DATA data) {
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
   curl_easy_setopt(curl, CURLOPT_URL, url);
-#ifdef __FreeBSD__
+
+//#ifdef __FreeBSD__
   curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1);
-#endif
+//#endif
 
   if (debug)
     printf("Request %s\n", url);
